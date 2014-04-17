d816cc0301776dca3a19e822b7fbc8c69e29ea67
leggetter <phil@leggetter.co.uk>
2014-04-17 23:13:14 +0100

    Add messages Blade to the Aspect

--- default-aspect/src/modularapp/App.js
+++ default-aspect/src/modularapp/App.js
@@ -5,18 +5,28 @@ var KnockoutComponent = require( 'br/knockout/KnockoutComponent' );
 // require Blades
 var HeaderViewModel = require( 'modularapp/chat/header/HeaderViewModel' );
 var InputViewModel = require( 'modularapp/chat/input/InputViewModel' );
+var MessagesViewModel = require( 'modularapp/chat/messages/MessagesViewModel' );
 
 var App = function() {
 
   // Create and add Header Blade
   var headerViewModel = new HeaderViewModel();
-  var headerComponent = new KnockoutComponent( 'modularapp.chat.header.view-template', headerViewModel );
+  var headerComponent =
+    new KnockoutComponent( 'modularapp.chat.header.view-template', headerViewModel );
   var headerEl = headerComponent.getElement();
   document.body.appendChild( headerEl );
 
+  // Create and add Messages Blade
+  var messagesViewModel = new MessagesViewModel();
+  var messagesComponent =
+    new KnockoutComponent( 'modularapp.chat.messages.view-template', messagesViewModel );
+  var messagesEl = messagesComponent.getElement();
+  document.body.appendChild( messagesEl );
+
   // Create and add Input Blade
   var inputViewModel = new InputViewModel();
-  var inputComponent = new KnockoutComponent( 'modularapp.chat.input.view-template', inputViewModel );
+  var inputComponent =
+    new KnockoutComponent( 'modularapp.chat.input.view-template', inputViewModel );
   var inputEl = inputComponent.getElement();
   document.body.appendChild( inputEl );
 
