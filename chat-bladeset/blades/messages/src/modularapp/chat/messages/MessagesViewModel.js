ce755a950d1d7ba495d056d7660c79cb5eac1c9d
leggetter <phil@leggetter.co.uk>
2014-04-17 22:15:43 +0100

    Basic view model, view generation and tests

--- chat-bladeset/blades/messages/src/modularapp/chat/messages/MessagesViewModel.js
+++ chat-bladeset/blades/messages/src/modularapp/chat/messages/MessagesViewModel.js
@@ -2,12 +2,19 @@
 
 var ko = require( 'ko' );
 
+var MessageItemViewModel = require( './MessageItemViewModel' );
+
 function MessagesViewModel() {
-	this.message = ko.observable( 'Hello World!' );
+	this.messages = ko.observableArray( [] );
+
+	// Temporarily add some fake data
+	this._addMessage( { userId: 'leggetter', text: 'hello', timestamp: new Date() } );
+	this._addMessage( { userId: 'andyberry88', text: 'yo', timestamp: new Date() } );
 }
 
-MessagesViewModel.prototype.buttonClicked = function() {
-	console.log( 'button clicked' );
-};
+MessagesViewModel.prototype._addMessage = function( message ) {
+	var model = new MessageItemViewModel( message );
+	this.messages.push( model );
+}
 
 module.exports = MessagesViewModel;
