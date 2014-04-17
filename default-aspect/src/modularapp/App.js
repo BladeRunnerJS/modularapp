e08cf27c06e664de44a81ab8c92d5606a2e36e71
leggetter <phil@leggetter.co.uk>
2014-04-17 20:13:50 +0100

    adding input Blade to Aspect

--- default-aspect/src/modularapp/App.js
+++ default-aspect/src/modularapp/App.js
@@ -4,13 +4,20 @@ var KnockoutComponent = require( 'br/knockout/KnockoutComponent' );
 
 // require Blades
 var HeaderViewModel = require( 'modularapp/chat/header/HeaderViewModel' );
+var InputViewModel = require( 'modularapp/chat/input/InputViewModel' );
 
 var App = function() {
+
   var headerViewModel = new HeaderViewModel();
   var headerComponent = new KnockoutComponent( 'modularapp.chat.header.view-template', headerViewModel );
   var headerEl = headerComponent.getElement();
-
   document.body.appendChild( headerEl );
+
+  var inputViewModel = new InputViewModel();
+  var inputComponent = new KnockoutComponent( 'modularapp.chat.input.view-template', inputViewModel );
+  var inputEl = inputComponent.getElement();
+  document.body.appendChild( inputEl );
+
 };
 
 module.exports = App;
