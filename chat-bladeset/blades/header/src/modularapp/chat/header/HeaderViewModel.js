a4d4b7df2e2133b0eda82381fec4f6fc7fe0375b
leggetter <phil@leggetter.co.uk>
2014-04-17 16:33:13 +0100

    Basic header added to default aspect

--- chat-bladeset/blades/header/src/modularapp/chat/header/HeaderViewModel.js
+++ chat-bladeset/blades/header/src/modularapp/chat/header/HeaderViewModel.js
@@ -1,13 +1,6 @@
 'use strict';
 
-var ko = require( 'ko' );
-
 function HeaderViewModel() {
-	this.message = ko.observable( 'Hello World!' );
 }
-
-HeaderViewModel.prototype.buttonClicked = function() {
-	console.log( 'button clicked' );
-};
 
 module.exports = HeaderViewModel;
