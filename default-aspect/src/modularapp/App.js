9f1da698eab135060182ea462a727239d5be9e50
leggetter <phil@leggetter.co.uk>
2014-04-17 16:33:13 +0100

    Basic header added to default aspect

--- default-aspect/src/modularapp/App.js
+++ default-aspect/src/modularapp/App.js
@@ -1,7 +1,16 @@
 'use strict';
 
+var KnockoutComponent = require( 'br/knockout/KnockoutComponent' );
+
+// require Blades
+var HeaderViewModel = require( 'modularapp/chat/header/HeaderViewModel' );
+
 var App = function() {
-  // your app code goes here
+  var headerViewModel = new HeaderViewModel();
+  var headerComponent = new KnockoutComponent( 'modularapp.chat.header.view-template', headerViewModel );
+  var headerEl = headerComponent.getElement();
+
+  document.body.appendChild( headerEl );
 };
 
 module.exports = App;
