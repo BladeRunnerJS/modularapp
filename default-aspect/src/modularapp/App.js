20c3004d6036dd41451cae0fdc278ae47a774006
leggetter <phil@leggetter.co.uk>
2014-04-17 22:14:04 +0100

    Comments

--- default-aspect/src/modularapp/App.js
+++ default-aspect/src/modularapp/App.js
@@ -8,11 +8,13 @@ var InputViewModel = require( 'modularapp/chat/input/InputViewModel' );
 
 var App = function() {
 
+  // Create and add Header Blade
   var headerViewModel = new HeaderViewModel();
   var headerComponent = new KnockoutComponent( 'modularapp.chat.header.view-template', headerViewModel );
   var headerEl = headerComponent.getElement();
   document.body.appendChild( headerEl );
 
+  // Create and add Input Blade
   var inputViewModel = new InputViewModel();
   var inputComponent = new KnockoutComponent( 'modularapp.chat.input.view-template', inputViewModel );
   var inputEl = inputComponent.getElement();
