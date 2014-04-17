f68b57c76f258d4c5c43d2fa2d96a5b657fec07d
leggetter <phil@leggetter.co.uk>
2014-04-17 18:14:00 +0100

    very basic chat input functionality

--- chat-bladeset/blades/input/src/modularapp/chat/input/InputViewModel.js
+++ chat-bladeset/blades/input/src/modularapp/chat/input/InputViewModel.js
@@ -3,11 +3,26 @@
 var ko = require( 'ko' );
 
 function InputViewModel() {
-	this.message = ko.observable( 'Hello World!' );
+	this.message = ko.observable( '' );
 }
 
 InputViewModel.prototype.buttonClicked = function() {
-	console.log( 'button clicked' );
+	var message = this.message();
+	var valid = messageValid( message );
+	if( valid ) {
+		// TODO: send message
+	}
+
+	console.log( 'sent message?', valid );
+	return valid;
 };
+
+function messageValid( message ) {
+	var valid = true;
+	if( !message || message.trim().length === 0 ) {
+		valid = false;
+	}
+	return valid;
+}
 
 module.exports = InputViewModel;
