fb2e86aa5617bc51b80f2b341e2d3d5e8975336b
leggetter <phil@leggetter.co.uk>
2014-04-19 15:29:34 +0100

    Basic ChatService being used by input Blade

--- libs/chatservice/tests/test-unit/js-test-driver/tests/DummyChatServiceTest.js
+++
@@ -1,8 +0,0 @@
-var DummyChatServiceTest = TestCase("DummyChatServiceTest");
-
-var DummyChatService = require( 'modularapp/chatservice/DummyChatService' );
-
-DummyChatServiceTest.prototype.testCreatingDummyChatServiceInstance = function() {
-	var service = new DummyChatService();
-	assertTrue( service !== undefined );
-};
