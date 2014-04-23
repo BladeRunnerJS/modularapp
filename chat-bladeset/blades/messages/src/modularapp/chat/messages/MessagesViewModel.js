7815c0c93aca209412379c73b6358095aa68cb2b
leggetter <phil@leggetter.co.uk>
2014-04-23 22:01:44 +0100

    Moving temp data out of model

--- chat-bladeset/blades/messages/src/modularapp/chat/messages/MessagesViewModel.js
+++ chat-bladeset/blades/messages/src/modularapp/chat/messages/MessagesViewModel.js
@@ -3,18 +3,24 @@
 var ko = require( 'ko' );
 
 var MessageItemViewModel = require( './MessageItemViewModel' );
+var ServiceRegistry = require( 'br/ServiceRegistry' );
 
 function MessagesViewModel() {
 	this.messages = ko.observableArray( [] );
 
-	// Temporarily add some fake data
-	this._addMessage( { userId: 'leggetter', text: 'hello', timestamp: new Date() } );
-	this._addMessage( { userId: 'andyberry88', text: 'yo', timestamp: new Date() } );
+	this._chatService = ServiceRegistry.getService( 'chat.service' );
+	this._chatService.getMessages( this );
 }
+
+MessagesViewModel.prototype.messagesRetrieved = function( messages ) {
+	messages.forEach( function( message ) {
+		this._addMessage( message );
+	}, this );
+};
 
 MessagesViewModel.prototype._addMessage = function( message ) {
 	var model = new MessageItemViewModel( message );
 	this.messages.push( model );
-}
+};
 
 module.exports = MessagesViewModel;
