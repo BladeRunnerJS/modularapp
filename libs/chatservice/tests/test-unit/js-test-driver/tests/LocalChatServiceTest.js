bfba8297e539a5ebe3c251e3a69526977485c02a
leggetter <phil@leggetter.co.uk>
2014-04-23 22:01:27 +0100

    Adding async getMessages

--- libs/chatservice/tests/test-unit/js-test-driver/tests/LocalChatServiceTest.js
+++ libs/chatservice/tests/test-unit/js-test-driver/tests/LocalChatServiceTest.js
@@ -1,4 +1,4 @@
-var LocalChatServiceTest = TestCase("LocalChatServiceTest");
+var LocalChatServiceTest = TestCase( 'LocalChatServiceTest' );
 
 var LocalChatService = require( 'chatservice/LocalChatService' );
 var ChatMessage = require( 'chatservice/ChatMessage' );
@@ -29,4 +29,29 @@ LocalChatServiceTest.prototype.testMessageObjectIsPassedWhenNewMessageIsTriggere
 	var message = new ChatMessage( 'testId', "test message", new Date() );
 	service.sendMessage( message );
 	assertEquals( message, triggeredMessage );
+};
+
+
+var LocalChatServiceAsyncTest = AsyncTestCase('LocalChatServiceAsyncTest');
+
+LocalChatServiceAsyncTest.prototype.testCanGetMessages = function( queue ) {
+	var service = new LocalChatService();
+	var message1 = { userId: 'leggetter', text: 'hello', timestamp: new Date() };
+	var message2 = { userId: 'andyberry88', text: 'Yo!', timestamp: new Date() };
+	service.sendMessage( message1 );
+	service.sendMessage( message2 );
+
+	queue.call( 'Step 1: make request for messges', function( callbacks ) {
+
+		var messagesRetrievedCallback = callbacks.add( function( messages ) {
+			assertEquals( message1, messages[ 0 ] );
+			assertEquals( message2, messages[ 1 ] );
+		} );
+
+		service.getMessages( {
+			messagesRetrieved: messagesRetrievedCallback
+		} );
+
+	} );
+
 };
