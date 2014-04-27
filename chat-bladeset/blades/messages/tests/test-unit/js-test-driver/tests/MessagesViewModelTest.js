55c1fc94ccd88c049dfd4b18a401bc803e069153
leggetter <phil@leggetter.co.uk>
2014-04-27 11:06:43 +0100

    Make addMessage public Remove unused code

--- chat-bladeset/blades/messages/tests/test-unit/js-test-driver/tests/MessagesViewModelTest.js
+++ chat-bladeset/blades/messages/tests/test-unit/js-test-driver/tests/MessagesViewModelTest.js
@@ -4,10 +4,9 @@ var MessagesViewModel = require( 'modularapp/chat/messages/MessagesViewModel' );
 
 MessagesViewModelTest.prototype.testAddingAMessageIncreasesMessageCountByOne = function() {
   var model = new MessagesViewModel();
-  var initialMessageCount = model.messages().length;
-  var expectedMessageCount = initialMessageCount + 1;
+  var expectedMessageCount = 1;
 
-  model._addMessage( { userId: 'test', text: 'test text', timestamp: new Date() } );
+  model.addMessage( { userId: 'test', text: 'test text', timestamp: new Date() } );
   assertEquals( expectedMessageCount, model.messages().length );
 };
 
