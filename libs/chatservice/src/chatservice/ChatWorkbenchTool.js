433657efb83bcaf93927342124d482e6d8210c11
james-shaw-turner <james.shaw.turner@gmail.com>
2014-04-28 15:49:43 +0100

    add user field to workbench chat tool

--- libs/chatservice/src/chatservice/ChatWorkbenchTool.js
+++ libs/chatservice/src/chatservice/ChatWorkbenchTool.js
@@ -3,6 +3,7 @@ var ServiceRegistry = require( 'br/ServiceRegistry' );
 
 function ChatWorkbenchTool( ) {
 	this.message = ko.observable();
+	this.user = ko.observable();
 	this._el = document.createElement("div");
 	this._component = new KnockoutComponent( 'chatservice.workbench-chat-tool', this );
 	this._el.appendChild( this._component.getElement() );
@@ -13,13 +14,10 @@ ChatWorkbenchTool.prototype.getElement = function() {
 	  return this._el;
 };
 
-ChatWorkbenchTool.prototype.receive = function() {
-	var msg =  { userId: 'leggetter', text: this.message(), timestamp: new Date() }
-	this._chatServiceFake.receiveMessage(msg);
-};
+
 
 ChatWorkbenchTool.prototype.send = function() {
-	var msg =  { userId: 'leggetter', text: this.message(), timestamp: new Date() }
+	var msg =  { userId: this.user(), text: this.message(), timestamp: new Date() }
 	this._chatServiceFake.sendMessage(msg);
 };
 module.exports = ChatWorkbenchTool;
