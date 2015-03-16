1d003bf8a6418933b15f44d9d53ab86ff49d5534
leggetter <phil@leggetter.co.uk>
2015-03-16 18:00:58 +0000

    Indent in closure

--- blades/input/tests/test-unit/js-test-driver/tests/InputViewModelTest.js
+++ blades/input/tests/test-unit/js-test-driver/tests/InputViewModelTest.js
@@ -1,65 +1,65 @@
 (function() {
 
-var InputViewModelTest = TestCase( 'InputViewModelTest' );
+  var InputViewModelTest = TestCase( 'InputViewModelTest' );
 
-var InputViewModel = require( 'modularapp/input/InputViewModel' );
-var ServiceRegistry = require( 'br/ServiceRegistry' );
+  var InputViewModel = require( 'modularapp/input/InputViewModel' );
+  var ServiceRegistry = require( 'br/ServiceRegistry' );
 
-var model = null;
-var serviceInitialised = false;
+  var model = null;
+  var serviceInitialised = false;
 
-InputViewModelTest.prototype.setUp = function() {
-    var user = { userId: 'testUser' };
-    
-    if (!serviceInitialised) {
-        var userService = ServiceRegistry.getService( 'user.service' );
-        // Set in order to stop exceptions from service
-        userService.setCurrentUser( user );
-        serviceInitialised = true;
-    }
+  InputViewModelTest.prototype.setUp = function() {
+      var user = { userId: 'testUser' };
+      
+      if (!serviceInitialised) {
+          var userService = ServiceRegistry.getService( 'user.service' );
+          // Set in order to stop exceptions from service
+          userService.setCurrentUser( user );
+          serviceInitialised = true;
+      }
 
-    model = new InputViewModel();
-    // set in order to all buttonClicked tests to be performed
-    model.userRetrieved( user );
-};
-
-// Note: ServiceRegistry is cleared down for each test by the test runtime.
-// So no need to tearDown manually.
-
-InputViewModelTest.prototype.testDefaultMessageIsEmpty = function() {
-  assertEquals( '', model.message() );
-};
-
-InputViewModelTest.prototype.testEmptyMessageIsInvalid = function() {
-  model.message( '' );
-  assertFalse( model.buttonClicked() );
-};
-
-InputViewModelTest.prototype.testBlankMessageIsInvalid = function() {
-  model.message( '         ' );
-  assertFalse( model.buttonClicked() );
-};
-
-InputViewModelTest.prototype.testMessageWithValidValueIsClearedAfterButtonClicked = function() {
-  model.message( 'hello' );
-  model.buttonClicked();
-
-  assertEquals( '', model.message() );
-};
-
-// Tests where the UserService hasn't been set up with a User
-var InputViewModelNoUserTest = TestCase( 'InputViewModelNoUserTest' );
-
-InputViewModelNoUserTest.prototype.testCurrentUserMustBeSetBeforeMessagesCanBeSent = function() {
-  var test = function() {
-    var noUserModel = new InputViewModel();
-
-    noUserModel.message( 'hello' );
-    noUserModel.buttonClicked();
+      model = new InputViewModel();
+      // set in order to all buttonClicked tests to be performed
+      model.userRetrieved( user );
   };
 
-  assertException( test, 'Error' );
+  // Note: ServiceRegistry is cleared down for each test by the test runtime.
+  // So no need to tearDown manually.
 
-};
+  InputViewModelTest.prototype.testDefaultMessageIsEmpty = function() {
+    assertEquals( '', model.message() );
+  };
+
+  InputViewModelTest.prototype.testEmptyMessageIsInvalid = function() {
+    model.message( '' );
+    assertFalse( model.buttonClicked() );
+  };
+
+  InputViewModelTest.prototype.testBlankMessageIsInvalid = function() {
+    model.message( '         ' );
+    assertFalse( model.buttonClicked() );
+  };
+
+  InputViewModelTest.prototype.testMessageWithValidValueIsClearedAfterButtonClicked = function() {
+    model.message( 'hello' );
+    model.buttonClicked();
+
+    assertEquals( '', model.message() );
+  };
+
+  // Tests where the UserService hasn't been set up with a User
+  var InputViewModelNoUserTest = TestCase( 'InputViewModelNoUserTest' );
+
+  InputViewModelNoUserTest.prototype.testCurrentUserMustBeSetBeforeMessagesCanBeSent = function() {
+    var test = function() {
+      var noUserModel = new InputViewModel();
+
+      noUserModel.message( 'hello' );
+      noUserModel.buttonClicked();
+    };
+
+    assertException( test, 'Error' );
+
+  };
 
 })();
